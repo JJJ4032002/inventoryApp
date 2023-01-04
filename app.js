8db59e187e78b7112dc98af24479d273db3bd30b
JJJ4032002 <rp4888888@gmail.com>
2023-01-04 21:40:02 +0530

    Add app

---
+++ app.js
@@ -0,0 +1,55 @@
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var inventoryRouter = require("./routes/inventory");
+const mongoose = require("mongoose");
+var Item = require("./models/item");
+var Category = require("./models/category");
+var app = express();
+const mongoDB =
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/inventory";
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+Category.find().exec(function (err, result) {
+  console.log(result);
+});
+Item.find().exec(function (err, result) {
+  console.log(result);
+});
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/inventory", inventoryRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
