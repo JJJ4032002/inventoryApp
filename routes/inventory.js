2dfa870ede71416568da58faf93e05e2d1860596
JJJ4032002 <rp4888888@gmail.com>
2023-01-11 21:38:56 +0530

    Update routes to accept files from inputs

--- routes/inventory.js
+++ routes/inventory.js
@@ -1,4 +1,7 @@
 var express = require("express");
+const multer = require("multer");
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 var router = express.Router();
 const itemController = require("../controllers/itemController");
 const categoryController = require("../controllers/categoryController");
@@ -9,15 +12,23 @@ router.get("/categories", categoryController.Categories);
 //Directs to the page which displays all the items
 router.get("/items", itemController.Items);
 //Directs to the new item form page
-router.get("/item/new", itemController.ItemFormGet);
+router.get("/item/new", upload.single("itemFile"), itemController.ItemFormGet);
 //Add a new Item
-router.post("/item/new", itemController.ItemFormPost);
+router.post(
+  "/item/new",
+  upload.single("itemFile"),
+  itemController.ItemFormPost
+);
 //Directs to the page which displays individual item details
 router.get("/item/:ID", itemController.ItemDetails);
 //Directs to item update page
 router.get("/item/:ID/update", itemController.ItemUpdateGet);
 //Updates an item
-router.post("/item/:ID/update", itemController.ItemUpdatePost);
+router.post(
+  "/item/:ID/update",
+  upload.single("itemFile"),
+  itemController.ItemUpdatePost
+);
 // Directs to the delete page of item.
 router.get("/item/:ID/delete", itemController.ItemDeleteGet);
 //Deletes the item
@@ -25,7 +36,11 @@ router.post("/item/:ID/delete", itemController.ItemDeletePost);
 //Directs to the update category page
 router.get("/category/:ID/update", categoryController.CategoryUpdateGet);
 //updates the category
-router.post("/category/:ID/update", categoryController.CategoryUpdatePost);
+router.post(
+  "/category/:ID/update",
+  upload.single("categoryFile"),
+  categoryController.CategoryUpdatePost
+);
 //Directs to the delete page of the category
 router.get("/category/:ID/delete", categoryController.CategoryDeleteGet);
 //Deletes the category
@@ -33,7 +48,11 @@ router.post("/category/:ID/delete", categoryController.CategoryDeletePost);
 //Direct to the create category form page
 router.get("/category/new", categoryController.CategoryFormGet);
 // Add the new category
-router.post("/category/new", categoryController.CategoryFormPost);
+router.post(
+  "/category/new",
+  upload.single("categoryFile"),
+  categoryController.CategoryFormPost
+);
 //Directs to the page which displays individual Category details
 router.get("/category/:ID", categoryController.CategoryDetails);
 
